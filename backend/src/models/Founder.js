26fcd3b70b09dcafb515f395bad7e25f980328ef
Mehfooz Kudchikar <mehfoozkudchikar@gmail.com>
2025-04-17 17:26:53 +0530

    yoyo

--- backend/src/models/Founder.js
+++ backend/src/models/Founder.js
@@ -5,11 +5,7 @@ const founderSchema = new mongoose.Schema({
   companyName: {type: String, required: true},
   bio: {type: String, required: true},
   websiteUrl: String,
-<<<<<<< HEAD
-  BusinessNumber: String,
-=======
   BussinessNumber: {type: String, required: true},
->>>>>>> 6f1ea110a734793dc707796422708f2f5cf65695
 }, { timestamps: true });
 
 export const Founder = mongoose.model('Founder', founderSchema);
